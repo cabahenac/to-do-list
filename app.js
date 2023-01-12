20341e46da71a2e4faec17172529c5c0535f3f3e
Cesar Bahena <cabahenac@gmail.com>
2023-01-11 19:37:09 -0600

    Agregué /work para una lista específica de pendientes de trabajo

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.use(express.static('public'));
 
 let port = 3000;
 
-let items = ['Comprar comida', 'Cocinar la comida', 'Comer la comida']
+let items = ['Comprar comida', 'Cocinar la comida', 'Comer la comida'];
+let workItems = [];
 
 
 app.get('/', (req, res) => {
@@ -21,16 +22,33 @@ app.get('/', (req, res) => {
 
     let day = today.toLocaleDateString('es-MX', options);
 
-    res.render('list', { day: day, items: items });
+    res.render('list', { listTitle: day, items: items });
+});
+
+
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: 'Pendientes de trabajo', items: workItems });
 });
 
 
 app.post('/', (req, res) => {
     let item = req.body.newItem;
+    if (req.body.list === 'Pendientes') {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+});
 
-    items.push(item);
 
-    res.redirect('/');
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+
+    res.redirect('/work');
 });
 
 
